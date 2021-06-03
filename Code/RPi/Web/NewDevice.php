c7a6be4a2281fa6cea851d9c958f0fc1ee170766
DavidBaldwin1015 <DavidBaldwinEMT@gmail.com>
2021-06-02 21:33:00 -0500

    More corrections

--- Code/RPi/Web/NewDevice.php
+++ Code/RPi/Web/NewDevice.php
@@ -14,17 +14,24 @@ if(!$con){
 	die("Connection failed: ".mysqli_connect_error());
 }
 
-$sql = $con->prepare("INSERT INTO Devices (idDevices, DeviceName, DeviceType, Active, IO, Status, Pin, AD) VALUES (null, ?, ?, 1, ?, ?, ?, ?)");
-$sql->bind_param("ssssis", $Name, $Type, $IO, $InitStat, $Pin, $AD);
-$sql->execute();
+//$sql = $con->prepare("INSERT INTO Devices (idDevices, DeviceName, DeviceType, Active, IO, Status, Pin, AD) VALUES (null, ?, ?, 1, ?, ?, ?, ?)");
+//$sql->bind_param("ssssis", $Name, $Type, $IO, $InitStat, $Pin, $AD);
+//$sql->execute();
 
-//Confirm the new record is there
-$result = mysqli_query($con, "SELECT idDevices FROM Devices WHERE DeviceName LIKE ".$Name." AND Pin LIKE ".$Pin);
-if(mysqli_num_rows($result) == 0){
-	echo json_encode(array("Status"=>2));
+$sql = "INSERT INTO Devices (idDevices, DeviceName, DeviceType, Active, IO, Status, Pin, AD) VALUES (null, '".$Name."', '".$Type."', 1, '".$IO."', '".$InitStat."', ".$Pin.", '".$AD."')");
+if($con->query($sql)===TRUE){
+	//Confirm the new record is there
+	$result = mysqli_query($con, "SELECT idDevices FROM Devices WHERE DeviceName LIKE ".$Name." AND Pin LIKE ".$Pin);
+	if(mysqli_num_rows($result) == 0){
+		echo json_encode(array("Status"=>2));
+	} else {
+		echo json_encode(array("Status"=>1));
+	}
 } else {
-	echo json_encode(array("Status"=>1));
+	echo json_encode(array("Status"=>3));
 }
+
+
 
 //Add code to mark pin in Pins table as used
 
