b57c28d4efa2a3e643570688d59acf48f8e75a44
DavidBaldwin1015 <DavidBaldwinEMT@gmail.com>
2021-06-02 21:18:28 -0500

    New error checking for adding database records

--- Code/RPi/Web/NewDevice.php
+++ Code/RPi/Web/NewDevice.php
@@ -16,11 +16,17 @@ if(!$con){
 
 $sql = $con->prepare("INSERT INTO Devices (idDevices, DeviceName, DeviceType, Active, IO, Status, Pin, AD) VALUES (null, ?, ?, 1, ?, ?, ?, ?)");
 $sql->bind_param("ssssis", $Name, $Type, $IO, $InitStat, $Pin, $AD);
-if($sql->execute()===TRUE){
-	echo json_encode(array("Status"=>1));
+$sql->execute();
+
+//Confirm the new record is there
+$result = mysqli_query($con, "SELECT idDevices FROM Devices WHERE DeviceName LIKE ".$Name." AND Pin LIKE ".$Pin);
+if(mysqli_num_rows($result) == 0){
+	echo json_encode(array("Status":2));
 } else {
-	echo json_encode(array("Status"=>2));
+	echo json_encode(array("Status":1));
 }
+
+//Add code to mark pin in Pins table as used
 
 $con->close();
 ?>
