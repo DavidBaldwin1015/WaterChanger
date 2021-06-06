7be56a6fa10e7140504312bcd152833d125c923a
DavidBaldwin1015 <DavidBaldwinEMT@gmail.com>
2021-06-05 19:11:37 -0500

    Added a python script for passing serial commands to Arduino

--- Code/RPi/Web/SerialControl.py
+++ Code/RPi/Web/SerialControl.py
@@ -7,9 +7,10 @@ command = sys.argv[1]
 if __name__ == '__main__':
 	print(command)
 	ser=serial.Serial('/dev/ttyACM0',9600, timeout=1)
-	ser.write((command+"\n").encode('ascii'))
+	ser.write((str(command)+"\n").encode('ascii'))
 	time.sleep(0.2)
 	response = ser.readline().decode('ascii').rstrip()
 	f=open('/home/pi/responselog.txt', 'w')
 	f.write(response)
 	f.close()
+	ser.close()
